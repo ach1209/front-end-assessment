c8b99c7379a944d050c2b2835c4833f239d68019
Anthony <ach0074@gmail.com>
2021-11-13 09:08:44 -0500

    Change let to const in index.js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ const arrow = document.querySelector('.arrow');
 const primaryInfo = document.querySelector('.primary-info');
 
 arrow.addEventListener('click', () => {
-  let xCoord = primaryInfo.getBoundingClientRect().x;
-  let yCoord = primaryInfo.getBoundingClientRect().y;
+  const xCoord = primaryInfo.getBoundingClientRect().x;
+  const yCoord = primaryInfo.getBoundingClientRect().y;
 
   window.scrollTo({ top: yCoord, left: xCoord, behavior: 'smooth'})
 });
